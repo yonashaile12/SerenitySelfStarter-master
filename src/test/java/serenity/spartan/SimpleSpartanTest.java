5db4550739a94b2aeb7fe5f79cb8aed42d842c18
git config --global user.name <yonashaile12@gmail.com>
2020-12-26 13:13:51 -0600

    add one spartan test

--- src/test/java/serenity/spartan/SimpleSpartanTest.java
+++ src/test/java/serenity/spartan/SimpleSpartanTest.java
@@ -3,13 +3,18 @@ package serenity.spartan;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static net.serenitybdd.rest.SerenityRest.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 @SerenityTest
 public class SimpleSpartanTest {
@@ -36,6 +41,28 @@ public class SimpleSpartanTest {
                 .contentType(ContentType.TEXT)
                 .body( is("Hello from Sparta") )
         ;
+
+        // Serenity's of way of generating some steps for verification
+        // in the report using Ensure class
+        Ensure.that("Make sure endpoint works ",
+                    response -> response
+                                .statusCode(200)
+                                .contentType(ContentType.TEXT)
+                                .body( is("Hello from Sparta") )
+                   );
+
+        Ensure.that("Success Response was received",
+                     thenResponse -> thenResponse.statusCode(200) )
+                .andThat("I got text response",
+                        blaResponse -> blaResponse.contentType(ContentType.TEXT))
+                .andThat("I got Hello from Sparta",
+                        vResponse ->vResponse.body(is("Hello from Sparta"))
+                )
+                .andThat("i got my response time within 2 seconds",
+                        vResponse->vResponse.time(lessThan(2L), TimeUnit.SECONDS))
+        ;
+
+
 
 
     }
